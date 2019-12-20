e0b00f845d6b3176ebb6da799e1f53c7fdac3ac8
silence_zhpf <silence_zhpf@aliyun.com>
2019-12-20 10:47:53 +0800

    :bulb: 添加源码注释
    为 maxN 文件添加注释

--- src/array/maxN.js
+++ src/array/maxN.js
@@ -1,3 +1,10 @@
+/**
+ * @author silence_zhpf
+ * @description 取数组中的前n个最大的值，并且按从大到小的顺序组成数组
+ * @param {number[]} array 需要取值的数组
+ * @param {number} [n=1]
+ * @returns {number[]}
+ */
 exports.maxN = function maxN(array, n = 1) {
   return Array.from(array).sort((a, b) => b - a).slice(0, n)
 };
