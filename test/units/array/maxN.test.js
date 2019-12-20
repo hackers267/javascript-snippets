7368847c11b7c39d63d6e161128c0283b5fe58d7
silence_zhpf <silence_zhpf@aliyun.com>
2019-12-20 16:50:36 +0800

    :ok_hand: 更新代码，因为代码审核变更
    使用 toHaveLength 来判断数组长度值

--- test/units/array/maxN.test.js
+++ test/units/array/maxN.test.js
@@ -4,7 +4,7 @@ describe('The maxN of Array', function () {
     const array = [3, 8, 19, 4, 5];
     const expected = [19];
     const result = maxN(array);
-    expect(result.length).toEqual(1);
+    expect(result).toHaveLength(1);
     expect(result).toEqual(expect.arrayContaining(expected));
   });
   test("The maxN of Array is 3", () => {
@@ -12,7 +12,7 @@ describe('The maxN of Array', function () {
     const expected = [19, 8, 5];
     const maxNumber = 3;
     const result = maxN(array, maxNumber);
-    expect(result.length).toEqual(maxNumber);
+    expect(result).toHaveLength(maxNumber);
     expect(result).toEqual(expect.arrayContaining(expected));
   })
 });
