b5b01ba240b09c0562d5c409f18a943b94290bea
silence_zhpf <silence_zhpf@aliyun.com>
2019-12-20 09:25:53 +0800

    :heavy_plus_sign: 添加依赖 添加JSDoc依赖
    添加JSDoc依赖，准备为各个文档生成函数

--- src/array/compact.js
+++ src/array/compact.js
@@ -1,3 +1,8 @@
+/**
+ * @description 去除数组中的假值，返回数组中由真值组陈的数组
+ * @param {Array} array 数组
+ * @returns {Array} 原数组中真值组成的数组
+ */
 exports.compact = function compact(array) {
   return array.filter(n => n)
 };
