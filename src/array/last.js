23de5185983ea4e96b6dba714495fe778b6a04cf
silence_zhpf <silence_zhpf@aliyun.com>
2019-12-20 10:29:11 +0800

    :bulb: 添加源码注释
    为 last 文件添加注释

--- src/array/last.js
+++ src/array/last.js
@@ -1,3 +1,9 @@
+/**
+ * @author silence_zhpf
+ * @description 获取数组中的最后一个元素
+ * @param {Array} array 数组
+ * @returns {*}
+ */
 exports.last = function last(array) {
   if (Array.isArray(array)) {
     const len = array.length;
