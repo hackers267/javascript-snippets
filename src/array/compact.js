2253c35a416177a0af8ed4707b71287141325f48
silence_zhpf <silence_zhpf@aliyun.com>
2019-12-20 10:20:22 +0800

    :bulb: 添加源码注释
    为 compact 和 head 文件添加注释

--- src/array/compact.js
+++ src/array/compact.js
@@ -1,4 +1,5 @@
 /**
+ * @author silence_zhpf
  * @description 去除数组中的假值，返回数组中由真值组陈的数组
  * @param {Array} array 数组
  * @returns {Array} 原数组中真值组成的数组
--- src/array/head.js
+++ src/array/head.js
@@ -1,3 +1,9 @@
+/**
+ * @author silence_zhpf
+ * @description 返回数组中的第一个元素，如果传入的不是数组，则直接返回 undefined
+ * @param array{Any} 数组，如果不是数组，则返回 undefined, null
+ * @returns {*} 数组中的第一个元素或者是 undefined
+ */
 exports.head = function head(array) {
   if (Array.isArray(array)) {
     return array[0]
