71b9330c1031ca7a4969ee5b3d353e390d70600c
silence_zhpf <silence_zhpf@aliyun.com>
2019-12-20 10:24:18 +0800

    :bulb: 添加源码注释
    为 includeAll 文件添加注释

--- src/array/includeAll.js
+++ src/array/includeAll.js
@@ -1,3 +1,10 @@
+/**
+ * @author silence_zhpf
+ * @description 判断第一个数组（父数组）中是否含有第二个数组（子数组）中的全部元素
+ * @param {Array} parentArray 父数组
+ * @param {Array} childArray 子数组
+ * @returns {boolean}
+ */
 exports.includeAll = function includeAll(parentArray, childArray) {
   return childArray.every(value => parentArray.includes(value))
 };
