7f756eb9eb6ea8d3bbc29505e2b7f5dc4291c60f
silence_zhpf <silence_zhpf@aliyun.com>
2019-12-20 11:57:01 +0800

    :sparkles: 添加新特性-数组的 reject 方法
    新增数组的 reject 方法

---
+++ src/array/reject.js
@@ -0,0 +1,18 @@
+exports.reject = reject;
+
+/**
+ * @callback pred 断言函数
+ * @param {*} args 数组过滤参数中对应的值 value,index,array
+ * @return boolean
+ */
+
+/**
+ * @author silence_zhpf
+ * @description 返回断言函数为真的元素组成的数组
+ * @param {Array} array 需要整理的数组
+ * @param {pred} pred 断言函数
+ * @return {Array} 过滤后的数组
+ */
+function reject(pred, array) {
+  return array.filter((...args) => pred(...args))
+}
