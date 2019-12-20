c596bf7308a0b873417527552de86a9732527736
silence_zhpf <silence_zhpf@aliyun.com>
2019-12-20 10:26:14 +0800

    :bulb: 添加源码注释
    为 includeAny 文件添加注释

--- src/array/includeAny.js
+++ src/array/includeAny.js
@@ -1,3 +1,10 @@
+/**
+ * @author silence_zhpf
+ * @description 判断第一个数组（父数组）中是否包含第二个数组（子数组）中的任意元素，存在->true,不存在->false
+ * @param {Array} parentArray 父数组
+ * @param {Array} childArray 子数组
+ * @returns {boolean}
+ */
 exports.includeAny = function includeAny(parentArray, childArray) {
   return childArray.some(value => parentArray.includes(value))
 };
