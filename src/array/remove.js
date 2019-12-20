5e3bed52335e2e9fd3abdf42cb745afc8109ca5e
silence_zhpf <silence_zhpf@aliyun.com>
2019-12-20 11:39:03 +0800

    :bulb: 添加源码注释
    为 remove 文件添加注释

--- src/array/remove.js
+++ src/array/remove.js
@@ -1,3 +1,16 @@
+/**
+ * @callback fn
+ * @param {*} value 输入值
+ * @return {boolean}
+ */
+
+/**
+ * @author silence_zhpf
+ * @description 根据断言函数(fn)移除数组中断言函数为true的数组，并返回新的数组
+ * @param {Array} arr 需要整理的数组
+ * @param {fn} fn
+ * @returns {Array}
+ */
 exports.remove = function remove(arr, fn) {
   return Array.isArray(arr) ? arr.filter(val => !fn(val)) : [];
 };
