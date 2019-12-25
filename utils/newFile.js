a28896ffd8086249bd76c58911dd2640ff263444
silence_zhpf <silence_zhpf@aliyun.com>
2019-12-25 18:47:14 +0800

    :sparkles: 添加新特性-优化控制台输出
    优化创建新文件控制台的输出

--- utils/newFile.js
+++ utils/newFile.js
@@ -1,6 +1,7 @@
 const { writeFileSync } = require("fs");
 const { resolve } = require("path");
 const { argv } = require("yargs");
+const chalk = require("chalk");
 
 const srcPath = resolve(__dirname, "../src/array");
 const testPath = resolve(__dirname, "../test/units/array");
@@ -11,6 +12,11 @@ const srcName = resolve(srcPath, `${fileName}.js`);
 try {
   writeFileSync(srcName, "");
   writeFileSync(testName, "");
+  console.info(
+    `成功生成文件： ${chalk.green(srcName)} 和测试文件 ${chalk.yellow(
+      testName
+    )}`
+  );
 } catch (e) {
   console.error("生成文件错误", e);
 }
