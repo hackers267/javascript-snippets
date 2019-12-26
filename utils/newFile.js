729bdca2ecac3a320feb6c6b6372421904e1f111
silence_zhpf <silence_zhpf@aliyun.com>
2019-12-26 17:10:11 +0800

    :sparkles: 添加新特性-为新增文件添加模板
    为生成新文件添加模板

--- utils/newFile.js
+++ utils/newFile.js
@@ -1,5 +1,6 @@
-const { writeFileSync } = require("fs");
+const { createReadStream, createWriteStream } = require("fs");
 const { resolve } = require("path");
+const { Transform, pipeline } = require("stream");
 const { argv } = require("yargs");
 const chalk = require("chalk");
 
@@ -8,10 +9,31 @@ const testPath = resolve(__dirname, "../test/units/array");
 const fileName = argv.file;
 const testName = resolve(testPath, `${fileName}.test.js`);
 const srcName = resolve(srcPath, `${fileName}.js`);
+const sourceSrcName = resolve(__dirname, "./template/array/src.txt");
+const sourceTestName = resolve(__dirname, "./template/array/test.txt");
+
+class MyTransform extends Transform {
+  // eslint-disable-next-line no-underscore-dangle
+  _transform(chunk, encoding, callback) {
+    const str = chunk.toString();
+    const replacer = /name/g;
+    const result = str.replace(replacer, fileName);
+    this.push(result);
+    callback();
+  }
+}
+function generateFile(source, destination) {
+  const sourceStream = createReadStream(source, { encoding: "utf-8" });
+  const destinationStream = createWriteStream(destination, {
+    encoding: "utf-8"
+  });
+  const transform = new MyTransform();
+  pipeline(sourceStream, transform, destinationStream);
+}
 
 try {
-  writeFileSync(srcName, "");
-  writeFileSync(testName, "");
+  generateFile(sourceSrcName, srcName);
+  generateFile(sourceTestName, testName);
   console.info(
     `成功生成文件： ${chalk.green(srcName)} 和测试文件 ${chalk.yellow(
       testName
